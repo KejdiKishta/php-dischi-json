efaf3ecd8efaca8b7f1fba5596c6e659b5a6e51e
Kejdi Kishta <155896977+KejdiKishta@users.noreply.github.com>
2024-05-23 15:08:14 +0200

    create: API

--- server.php
+++ server.php
@@ -1,1 +1,15 @@
 <?php
+// nella variable metto una stringa con il contenuto del file json
+$disc_list = file_get_contents("dischi.json");
+// var_dump($disc_list);
+
+// codice per trasformare la JSON string in array
+// $disc_list_to_array = json_decode($disc_list, true);
+// var_dump($disc_list_to_array);
+
+// e viceversa
+// $disc_list_to_json = json_encode($disc_list_to_array);
+
+header("Content-Type: application/json");
+
+echo $disc_list;
