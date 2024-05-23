065aef1ce5c7aee1716c51bfe7ace424ba14937f
Kejdi Kishta <155896977+KejdiKishta@users.noreply.github.com>
2024-05-23 15:54:07 +0200

    add: cards in DOM

--- script.js
+++ script.js
@@ -3,7 +3,15 @@ const { createApp } = Vue
 createApp({
     data() {
         return {
-
+            discs: [],
         }
+    },
+    created() {
+        axios
+        .get("http://localhost/boolean/php-dischi-json/server.php")
+        .then((resp) => {
+            console.log(resp);
+            this.discs = resp.data
+        })
     }
 }).mount('#app')
