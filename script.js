34ac7ef2f66c4f5f961bc9cb9845391b0633c20c
Kejdi Kishta <155896977+KejdiKishta@users.noreply.github.com>
2024-05-24 17:51:07 +0200

    add: like buton

--- script.js
+++ script.js
@@ -11,7 +11,26 @@ createApp({
         .get("http://localhost/boolean/php-dischi-json/server.php")
         .then((resp) => {
             console.log(resp);
-            this.discs = resp.data.result
+            this.discs = resp.data
         })
+    },
+    methods: {
+        toggleLike(index) {
+            const data = {
+                action: "toggle-like",
+                like_index: index,
+            }
+
+            axios
+            .post("http://localhost/boolean/php-dischi-json/server.php", data, {
+                headers: {
+                    "Content-Type": "multipart/form-data",
+                }
+            })
+            .then((resp) => {
+                console.log(resp);
+                this.discs = resp.data
+            })
+        }
     }
 }).mount('#app')
--- server.php
+++ server.php
@@ -1,4 +1,8 @@
 <?php
+// 1. Aggiungete ad ogni disco la chiave "like" con valore booleano che rappresenta se l'utente ha aggiunto il disco ai preferiti.
+// Al click sul bottone per ogni disco fare una chiamata api che cambia il valore del like nel file json e invia l'array di dischi aggiornati da stampare in pagina.
+// 2. Permettere all'utente di selezionare se visualizzare tutti i dischi o solo quelli preferiti. Il filtro deve essere applicato lato server. Quindi il client invia la richiesta al server con il valore del filtro, il server prepara l'array di dischi in base al filtro e lo invia al client.
+
 // nella variable metto una stringa con il contenuto del file JSON
 $disc_list = file_get_contents("dischi.json");
 // var_dump($disc_list);
@@ -7,14 +11,14 @@ $disc_list = file_get_contents("dischi.json");
 $disc_list_to_array = json_decode($disc_list, true);
 // var_dump($disc_list_to_array);
 
-// strutturiamo il nostro array associativo
-$response = [
-    "result" => $disc_list_to_array,
-    "success" => true,
-];
+if (isset($_POST["action"]) && $_POST["action"] === "toggle-like") {
+    $index = $_POST["like_index"];
+    $disc_list_to_array[$index]["like"] = !$disc_list_to_array[$index]["like"];
+    file_put_contents("dischi.json", json_encode($disc_list_to_array));
+}
 
 // codice per trasformare la array string in JSON
-$response_to_json = json_encode($response);
+$response_to_json = json_encode($disc_list_to_array);
 
 // facciamo leggere il file come json
 header("Content-Type: application/json");
